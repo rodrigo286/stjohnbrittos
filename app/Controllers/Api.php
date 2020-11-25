7f606d7bed4062e9b2a7d7fa6b399457cac07489
Rodrigo Ventura <rodrigov867@gmail.com>
2020-11-25 20:26:05 -0300

    inicial

---
+++ app/Controllers/Api.php
@@ -0,0 +1,15 @@
+<?php namespace App\Controllers;
+
+class Api extends BaseController
+{
+	public function getMenuItens()
+	{
+		echo 'teste';
+	}
+
+	public function ola()
+	{
+		echo 'O jogo...<br/><br/>';
+		echo 'Você perdeu !';
+	}
+}
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="Rodrigo V. Rodrigues">
+		<title>St John Brittos - Delivery</title>
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+			}
+		</style>
+
+	</head>
+
+	<body>
+		<div class="text-center align-middle" style="margin-top: 10%;">
+			<p style="font-size: 40px; font-weight: bold; font-family: Calibri;">St John Britto's Delivery CI4 PHP API</p>
+			<img src="assets/images/logo-stjohnbritto.png" class="img-fluid">
+		</div>
+
+		<footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
+			<div class="text-center align-middle">
+				<img class="mb-2" src="assets/images/ci4.png"" alt="" width="24" height="24">
+				<small class="d-block mb-3 text-muted">Grupo 10 &copy; 2020-2021</small>
+			</div>
+		</footer>
+	</body>
+</html>
